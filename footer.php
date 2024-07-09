f3a2c8def4bb861efa8033581f2660f80ad38b2c
Zirimabagabo Anslem <zbagabo@gmail.com>
2024-07-09 10:23:30 +0300

    added font awesome icons

--- footer.php
+++ footer.php
@@ -1,48 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer Example</title>
+    <!-- Font Awesome CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+</head>
+<body>
+    <div class='footer'>
+        <div class='row'>
+            <div class='col-md-3'>
+                <div class='footerLabel'>About BNPL</div>
+                <div class='footerLabelDesc'>
+                    BNPL is a crediting service that allows you to get served even when you have no money at the moment. Feel free to get served and pay back later.
+                </div>
+            </div>
 
-<div class='footer'>
-    
-<div class='row'>
-<div class='col-md-3'>
-<div class='footerLabel'>About BNPL</div>
-<div class='footerLabelDesc'>BNPL is a crediting service that alows you get served even when you have no money at te moment. <p></p>
-Feel free toget served and pay back later.
+            <div class='col-md-3'>
+                <div class='footerLabel'> <i class='fas fa-map-marker-alt'></i> Location</div>
+                <div class='footerLabelDesc'>
+                    Located along Makerere Western gate, next to Baskon hostel.
+                </div>
+            </div>
 
-</div>
-</div>
+            <div class='col-md-3'>
+                <div class='footerLabel'>Contact</div>
+                <div class='footerLabelDesc'>
+                    <i class='fas fa-phone'></i> Tel: 0703852178 <br>
+                    <i class='fas fa-envelope'></i> Email: buynowpaylater@gmail.com
+                </div>
+            </div>
 
-<div class='col-md-3'>
-<div class='footerLabel'>Location</div>
-<div class='footerLabelDesc'>
-Loated along MakerereWestern gate, next ot Baskon hostel
-</div>
-</div>
+            <div class='col-md-3'>
+                <div class='footerLabel'>Social media</div>
+                <div class='footerLabelDesc'>
+                    <i class='fab fa-twitter'></i> Twitter: @bnpl <br>
+                    <i class='fab fa-facebook'></i> Facebook: buynowpaylater <br>
+                    <i class='fab fa-whatsapp'></i> WhatsApp: 0703852178
+                </div>
+            </div>
+        </div>
+    </div>
 
-<div class='col-md-3'>
-<div class='footerLabel'>Contact</div>
-<div class='footerLabelDesc'>
-Tel: 0703852178 <br>
-Email: buynowpaylater@gmail.com
-</div>
-</div>
-
-<div class='col-md-3'>
-<div class='footerLabel'>Social media</div>
-<div class='footerLabelDesc'>
-Twitter: @bnpl <br>Facebook: buynowpaylater <br>
-WhatsApp: 0703852178
-</div>
-</div>
-
-
-</div>
-
-
-</div>
-
-
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.6.0/dist/umd/popper.min.js" integrity="sha384-KsvD1yqQ1/1+IA7gi3P0tyJcT3vR+NdBTt13hSJ2lnve8agRGXTTyNaBYmCR/Nwi" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
-   <script src='./components/functions.js'>
-</script>
+    <script src='./components/functions.js'></script>
 </body>
 </html>
