b3e17096893ba992b1dc3558e396538e8a41a524
Zirimabagabo Anslem <zbagabo@gmail.com>
2024-07-12 11:58:58 +0300

    configured validations in signup.php

--- signup.php
+++ signup.php
@@ -9,12 +9,11 @@ include './header.php';
 <div style='padding-top:50px;' class='col-md-6'>
 
 <div class='formContainer' >
-   
+
 
 <?php
     if(isset($_SESSION['alert'])){
 
-     
 ?>
 
 
@@ -25,11 +24,11 @@ include './header.php';
 
 
 
-<?php 
+<?php
 unset($_SESSION['alert_type']);
 unset($_SESSION['alert_msg']);
 }
-?> 
+?>
 
 <form id="signupForm" method = "POST" action="./components/handleFormRequests.php">
 <div class='formLabel'>Sign up</div>
@@ -44,7 +43,7 @@ unset($_SESSION['alert_msg']);
 <div class='formInputLabel'>Confirm password</div>
 <input type="text" name="password2" class='inputHoverEffect1'><br></br>
 <input type="hidden" name="signupForm" class='inputHoverEffect1'>
-<div class='button1'>Sign up</div>
+<div class='button1' onclick="ValidateSignUpForm()" >Sign up</div>
 
 </form></div>
 
@@ -52,5 +51,56 @@ unset($_SESSION['alert_msg']);
 </div>
 <div class='col-md-3'></div>
 </div>
+<script>
+function ValidateSignUpForm() {
+    const form = document.querySelector('#signupForm');
+    const name = form.name.value.trim();
+    const contact = form.contact.value.trim();
+    const email = form.email.value.trim();
+    const password = form.password.value.trim();
+    const password2 = form.password2.value.trim();
 
+
+    if (name === '') {
+        ToastAlert("toastAlert2", "Full Name is required", 2000);
+        return false;
+    }
+    if (contact === '') {
+        ToastAlert("toastAlert2", "Contact is required", 2000);
+        return false;
+    }
+    if (!/^\d{10}$/.test(contact)) {
+        ToastAlert("toastAlert2", "Contact must be exactly 10 digits", 2000);
+        return false;
+    }
+    if (email === '') {
+        ToastAlert("toastAlert2", "Email is required", 2000);
+        return false;
+    }
+    if (password === '') {
+        ToastAlert("toastAlert2", "Password is required", 2000);
+        return false;
+    }
+    if (password.length < 8) {
+        ToastAlert("toastAlert2", "Password must be at least 8 characters long", 2000);
+        return false;
+    }
+    if (!/\d/.test(password) || !/[!@#$%^&*]/.test(password)) {
+        ToastAlert("toastAlert2", "Password must contain at least one number and one symbol", 2000);
+        return false;
+    }
+    if (password2 === '') {
+        ToastAlert("toastAlert2", "Confirm password is required", 2000);
+        return false;
+    }
+    if (password !== password2) {
+        ToastAlert("toastAlert2", "Passwords do not match", 2000);
+        return false;
+    }
+
+    ToastAlert("toastAlert1", "Successful", 2000);
+        form.submit();
+}
+
+</script>
 <?php include 'footer.php'; ?>
