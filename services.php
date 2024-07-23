281d8983a33c608c9facd2d7ec291bcb8a8cc287
kayas111 <onongeisaac@gmail.com>
2024-07-23 15:16:21 +0300

    Connected services page to db

--- services.php
+++ services.php
@@ -1,7 +1,34 @@
+<?php include './connection.php'; ?>
 <?php include './header.php'; ?>
 
+<?php
+
+$result=mysqli_query($conn,'select * from business;');
+if (mysqli_num_rows($result)<1){
 
 
+
+
+?>
+
+
+<div>No services</div>
+
+<?php
+} else{
+    
+    $row=mysqli_fetch_assoc($result);
+
+echo json_encode($row);
+?>
+<div><?=$row['businessName']?></div>
+<div><?=$row['serviceDescription']?></div>
+<div><?=$row['bill']?></div>
+<div><?=$row['location']?></div>
+
+<?php
+}
+?>
 
 
 
